4b0a3036507d2fc28d7b5393cdd00b48b4aa2999
Hargun Bedi <hargunbedi@Harguns-MacBook-Pro.local>
2022-08-24 22:32:57 -0400

    initial commit

---
+++ screens/Home.js
@@ -0,0 +1,21 @@
+import { View, Text, SafeAreaView, ScrollView} from 'react-native'
+import React from 'react'
+import HeaderTabs from '../components/HeaderTabs'
+import SearchBar from '../components/SearchBar'
+import Categories from '../components/Categories'
+import RestaurantItem from '../components/RestaurantItem'
+
+export default function Home() {
+  return (
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1}}>
+        <View style={{backgroundColor: "white", padding: 15}}>
+            <HeaderTabs />
+            <SearchBar/>
+        </View>
+        <ScrollView vertical showsVerticalScrollIndicator={false}>
+            <Categories />
+            <RestaurantItem />
+        </ScrollView>
+    </SafeAreaView>
+  )
+}
