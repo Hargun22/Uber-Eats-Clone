353ca977fa424400cba096812d0f6f73f1079ff2
Hargun Bedi <hargunbedi@Harguns-MacBook-Pro.local>
2022-08-24 23:56:06 -0400

    Added yelp api

---
+++ components/RestaurantItems.js
@@ -0,0 +1,91 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+export const localRestaurants = [
+  {
+    name: "Beachside Bar",
+    image_url:
+      "https://media.istockphoto.com/photos/table-top-view-of-spicy-food-picture-id1316145932?b=1&k=20&m=1316145932&s=170667a&w=0&h=feyrNSTglzksHoEDSsnrG47UoY_XX4PtayUPpSMunQI=",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 1244,
+    rating: 4.5,
+  },
+  {
+    name: "Indian Bar",
+    image_url:
+      "https://media.istockphoto.com/photos/table-top-view-of-spicy-food-picture-id1316145932?b=1&k=20&m=1316145932&s=170667a&w=0&h=feyrNSTglzksHoEDSsnrG47UoY_XX4PtayUPpSMunQI=",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 1244,
+    rating: 4.5,
+  },
+  {
+    name: "Nirvana",
+    image_url:
+      "https://media.istockphoto.com/photos/table-top-view-of-spicy-food-picture-id1316145932?b=1&k=20&m=1316145932&s=170667a&w=0&h=feyrNSTglzksHoEDSsnrG47UoY_XX4PtayUPpSMunQI=",
+    categories: ["Indian", "Bar"],
+    price: "$",
+    reviews: 1244,
+    rating: 4.9,
+  },
+];
+
+export default function RestaurantItems({ restaurantData }) {
+  return (
+    <TouchableOpacity activeOpacity={1} style={{ marginBottom: 30 }}>
+      {restaurantData.map((restaurant, index) => (
+        <View
+          key={index}
+          style={{ marginTop: 10, padding: 15, backgroundColor: "white" }}
+        >
+          <RestaurantImage image={restaurant.image_url} />
+          <RestaurantInfo name={restaurant.name} rating={restaurant.rating} />
+        </View>
+      ))}
+    </TouchableOpacity>
+  );
+}
+
+const RestaurantImage = (props) => (
+  <>
+    <Image
+      source={{
+        uri: props.image,
+      }}
+      style={{ width: "100%", height: 180 }}
+    />
+    <TouchableOpacity style={{ position: "absolute", right: 20, top: 20 }}>
+      <MaterialCommunityIcons name="heart-outline" size={25} color="#fff" />
+    </TouchableOpacity>
+  </>
+);
+
+const RestaurantInfo = (props) => (
+  <View
+    style={{
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginTop: 10,
+    }}
+  >
+    <View>
+      <Text style={{ fontSize: 15, fontWeight: "bold" }}>{props.name}</Text>
+      <Text style={{ fontSize: 13, color: "gray" }}>30-45 • min</Text>
+    </View>
+    <View
+      style={{
+        backgroundColor: "#eee",
+        height: 30,
+        width: 30,
+        alignItems: "center",
+        borderRadius: 15,
+      }}
+    >
+      <Text>{props.rating}</Text>
+    </View>
+  </View>
+);
--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,47 @@
-import { View, Text, SafeAreaView, ScrollView} from 'react-native'
-import React from 'react'
-import HeaderTabs from '../components/HeaderTabs'
-import SearchBar from '../components/SearchBar'
-import Categories from '../components/Categories'
-import RestaurantItem from '../components/RestaurantItem'
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
+import Categories from "../components/Categories";
+import RestaurantItems, {
+  localRestaurants,
+} from "../components/RestaurantItems";
+
+const YELP_API_KEY =
+  "RwP1a9_UM955KMP-eXUp9avcXFBXa2UaSidCndKxTVsgHLhxKcAPb9NjKLIuxRc5RHQdancu3szRv2Jo9YUyIN67-5xXM8LUXNBJN9cRqO3Z0bpOgMGIElStZ-0GY3Yx";
 
 export default function Home() {
+  const [restaurantData, setRestaurantData] = useState(localRestaurants);
+
+  const getRestaurantsFromYelp = () => {
+    const yelpUrl =
+      "https://api.yelp.com/v3/businesses/search?term=restaurants&location=SanDiego";
+
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
+
+    return fetch(yelpUrl, apiOptions)
+      .then((res) => res.json())
+      .then((json) => setRestaurantData(json.businesses));
+  };
+
+  useEffect(() => {
+    getRestaurantsFromYelp();
+  }, []);
+
   return (
-    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1}}>
-        <View style={{backgroundColor: "white", padding: 15}}>
-            <HeaderTabs />
-            <SearchBar/>
-        </View>
-        <ScrollView vertical showsVerticalScrollIndicator={false}>
-            <Categories />
-            <RestaurantItem />
-        </ScrollView>
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+      <ScrollView vertical showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItems restaurantData={restaurantData} />
+      </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
