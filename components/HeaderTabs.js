b2e88106940fea067d20a11aa793434bbf90463a
Hargun22 <hargunbedi463@gmail.com>
2022-08-26 20:43:46 -0400

    added search bar functionali

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -1,44 +1,44 @@
-import { View, Text } from 'react-native'
-import React, {useState} from 'react'
-import { TouchableOpacity } from 'react-native'
+import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { TouchableOpacity } from "react-native";
 
-export default function HeaderTabs() {
-    const [activeTab, setActiveTab] = useState("Delivery");
+export default function HeaderTabs(props) {
+  //const [activeTab, setActiveTab] = useState("Delivery");
   return (
-    <View style = {{flexDirection: "row", alignSelf: "center"}}>
-      <HeaderButton 
-      text="Delivery" 
-      activeTab={activeTab}
-      setActiveTab={setActiveTab}
+    <View style={{ flexDirection: "row", alignSelf: "center" }}>
+      <HeaderButton
+        text="Delivery"
+        activeTab={props.activeTab}
+        setActiveTab={props.setActiveTab}
       />
 
-      <HeaderButton 
-      text="Pickup" 
-      activeTab={activeTab}
-      setActiveTab={setActiveTab}
+      <HeaderButton
+        text="Pickup"
+        activeTab={props.activeTab}
+        setActiveTab={props.setActiveTab}
       />
     </View>
-  )
+  );
 }
 
 const HeaderButton = (props) => (
-    <TouchableOpacity 
-        style ={{
-            backgroundColor: props.activeTab === props.text ? "black":"white", 
-            paddingVertical: 6,
-            paddingHorizontal: 16,
-            borderRadius: 30
+  <TouchableOpacity
+    style={{
+      backgroundColor: props.activeTab === props.text ? "black" : "white",
+      paddingVertical: 6,
+      paddingHorizontal: 16,
+      borderRadius: 30,
     }}
-    onPress = {() => props.setActiveTab(props.text)}
+    onPress={() => props.setActiveTab(props.text)}
+  >
+    <Text
+      style={{
+        color: props.activeTab === props.text ? "white" : "black",
+        fontSize: 15,
+        fontWeight: "900",
+      }}
     >
-        <Text
-            style={{
-                color: props.activeTab === props.text ? "white":"black", 
-                fontSize: 15, 
-                fontWeight: "900"
-            }}
-        >
-            {props.text}
-        </Text>
-    </TouchableOpacity>
+      {props.text}
+    </Text>
+  </TouchableOpacity>
 );
